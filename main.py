0358a53a2b5e31a3f5455f5fbe8d30ccaaa08793
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-04-23 02:43:19 +0300

    add table printing as output

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ from statistics import mean
 from itertools import count
 import time
 from hh_salaries import get_hh_data_by_language
+from superjob_vacancies import get_sj_data_by_language
+from terminaltables import AsciiTable
+
+
+def add_to_table(table_data, data_by_language):
+    table_data.append(
+        [
+            language,
+            data_by_language['vacancies_found'],
+            data_by_language['vacancies_processed'],
+            data_by_language['average_salary']
+        ]
+    )
 
 
 if __name__ == '__main__':
@@ -21,7 +34,19 @@ if __name__ == '__main__':
     ]
     data_by_languages = {}
 
-    for language in languages:
-        data_by_languages[language] = get_hh_data_by_language(language)
+    hh_table_data = [['Язык программирования', 'Вакансий найдено',
+                      'Вакансий обработано', 'Средняя зарплата']]
+    sj_table_data = hh_table_data.copy()
 
-    print(json.dumps(data_by_languages, indent=4))
+    for language in languages:
+        add_to_table(hh_table_data, get_hh_data_by_language(language))
+        add_to_table(sj_table_data, get_sj_data_by_language(language))
+
+    table = AsciiTable(hh_table_data)
+    table.title = 'HeadHunter Moscow'
+    print(table.table)
+
+    # print(json.dumps(data_by_languages, indent=4))
+    table = AsciiTable(sj_table_data)
+    table.title = 'SuperJob Moscow'
+    print(table.table)
